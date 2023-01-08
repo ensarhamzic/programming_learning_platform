34a92138636d76ff1823dbd44b691ef6670d0ad8
Ensar <ensarhamzic01@gmail.com>
2023-01-08 20:28:36 +0100

    initial commit

---
+++ app/Models/User.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'JMBG',
+        'role_id',
+        'name',
+        'surname',
+        'gender',
+        'username',
+        'birth_place',
+        'birth_country',
+        'birth_date',
+        'mobile_number',
+        'image_url',
+        'email',
+        'password',
+    ];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
---
+++ database/migrations/2023_10_12_000000_create_users_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->string("JMBG", 13)->primary();
+            $table->unsignedBigInteger('role_id');
+            $table->string('name');
+            $table->string('surname');
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->char("gender", 1);
+            $table->string("birth_place");
+            $table->string('birth_country');
+            $table->date("birth_date");
+            $table->string("mobile_number");
+            $table->string("image_url")->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};

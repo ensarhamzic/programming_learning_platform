f77e5baffd915532d19c13d768c8503d12a74dde
Ensar <ensarhamzic01@gmail.com>
2023-01-29 18:14:56 +0100

    final

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -32,36 +32,36 @@ class UserSeeder extends Seeder
             'approved' => true
         ]);
 
-        User::create([
-            "JMBG" => "1000000000000",
-            "role_id" => 3,
-            "name" => "Ensar",
-            "surname" => "Hamzic",
-            'username' => 'ensarhamzic',
-            'email' => "ensar@ensar.com",
-            'gender' => 'M',
-            'birth_place' => 'Admin',
-            'birth_country' => 'Admin',
-            'birth_date' => '2023-01-01',
-            'mobile_number' => '+381654487548',
-            'password' => Hash::make("admin123"),
-            'approved' => true
-        ]);
+        // User::create([
+        //     "JMBG" => "1000000000000",
+        //     "role_id" => 3,
+        //     "name" => "Ensar",
+        //     "surname" => "Hamzic",
+        //     'username' => 'ensarhamzic',
+        //     'email' => "ensar@ensar.com",
+        //     'gender' => 'M',
+        //     'birth_place' => 'Admin',
+        //     'birth_country' => 'Admin',
+        //     'birth_date' => '2023-01-01',
+        //     'mobile_number' => '+381654487548',
+        //     'password' => Hash::make("admin123"),
+        //     'approved' => true
+        // ]);
 
-        User::create([
-            "JMBG" => "1100000000000",
-            "role_id" => 2,
-            "name" => "Prof",
-            "surname" => "Prof",
-            'username' => 'profprof',
-            'email' => "prof@prof.com",
-            'gender' => 'M',
-            'birth_place' => 'prof',
-            'birth_country' => 'prof',
-            'birth_date' => '2023-01-01',
-            'mobile_number' => '+391621587245',
-            'password' => Hash::make("admin123"),
-            'approved' => true
-        ]);
+        // User::create([
+        //     "JMBG" => "1100000000000",
+        //     "role_id" => 2,
+        //     "name" => "Prof",
+        //     "surname" => "Prof",
+        //     'username' => 'profprof',
+        //     'email' => "prof@prof.com",
+        //     'gender' => 'M',
+        //     'birth_place' => 'prof',
+        //     'birth_country' => 'prof',
+        //     'birth_date' => '2023-01-01',
+        //     'mobile_number' => '+391621587245',
+        //     'password' => Hash::make("admin123"),
+        //     'approved' => true
+        // ]);
     }
 }
